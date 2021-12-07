363a0585f1088f7ab09a508ca285f787b8622988
Aaron Kling <webgeek1234@gmail.com>
2021-12-07 12:14:23 -0600

    Add libphs shim
    Change-Id: Ibcecc156b118c6726e4db6762f880a89bec8a486

--- shims/Android.mk
+++ shims/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+T124_SHIMS := $(LOCAL_PATH)/../../t124-common/shims
 
 PROTOBUF_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libprotobuf-cpp-lite-3.9.1.so
 $(PROTOBUF_SYMLINK):
@@ -6,6 +7,13 @@ $(PROTOBUF_SYMLINK):
 	$(hide) ln -sf /system/lib/libprotobuf-cpp-lite.so $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(PROTOBUF_SYMLINK)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := libphs
+LOCAL_SRC_FILES        := $(T124_SHIMS)/dummy.cpp
+LOCAL_SHARED_LIBRARIES := libnvphs
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE           := libw
